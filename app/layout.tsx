0a7ad7e5c36500cc7fb061446a474f86cf046ce3
Mingyi <963199927@qq.com>
2025-04-16 11:11:48 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,10 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 import { getServerSideConfig } from "./config/server";
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "MingyiChat",
+  description: "People's Chat Bot.",
   appleWebApp: {
-    title: "NextChat",
+    title: "MingyiChat",
     statusBarStyle: "default",
   },
 };
